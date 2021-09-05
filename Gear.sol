c3d2e3a1879da9dfa1dc683c57f1b39df5b7bd48
Ryan <ryanicale@gmail.com>
2021-09-05 17:38:30 +0800

    Step 7 and 8: ownerURI and constructor

--- Gear.sol
+++ Gear.sol
@@ -1393,7 +1393,7 @@ contract Gear is ERC721Enumerable, ReentrancyGuard, Ownable {
         return pluck(tokenId, "HEAD", headWear);
     }
 
-    function getlegWear(uint256 tokenId) public view returns (string memory) {
+    function getLegWear(uint256 tokenId) public view returns (string memory) {
         return pluck(tokenId, "LEG", legWear);
     }
     
@@ -1431,44 +1431,44 @@ contract Gear is ERC721Enumerable, ReentrancyGuard, Ownable {
 
     function tokenURI(uint256 tokenId) override public view returns (string memory) {
         string[17] memory parts;
-        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
+        parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: monospace; font-size: 14px; }</style><rect width="100%" height="100%" fill="#A663CC" /><text x="10" y="20" class="base">';
 
         parts[1] = getWeapon(tokenId);
 
         parts[2] = '</text><text x="10" y="40" class="base">';
 
-        parts[3] = getChest(tokenId);
+        parts[3] = getOuterChest(tokenId);
 
         parts[4] = '</text><text x="10" y="60" class="base">';
 
-        parts[5] = getHead(tokenId);
+        parts[5] = getInnerChest(tokenId);
 
         parts[6] = '</text><text x="10" y="80" class="base">';
 
-        parts[7] = getWaist(tokenId);
+        parts[7] = getHeadWear(tokenId);
 
         parts[8] = '</text><text x="10" y="100" class="base">';
 
-        parts[9] = getFoot(tokenId);
+        parts[9] = getLegWear(tokenId);
 
         parts[10] = '</text><text x="10" y="120" class="base">';
 
-        parts[11] = getHand(tokenId);
+        parts[11] = getFootWear(tokenId);
 
         parts[12] = '</text><text x="10" y="140" class="base">';
 
-        parts[13] = getNeck(tokenId);
+        parts[13] = getEyeWear(tokenId);
 
         parts[14] = '</text><text x="10" y="160" class="base">';
 
-        parts[15] = getRing(tokenId);
+        parts[15] = getCyberWare(tokenId);
 
         parts[16] = '</text></svg>';
 
         string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8]));
         output = string(abi.encodePacked(output, parts[9], parts[10], parts[11], parts[12], parts[13], parts[14], parts[15], parts[16]));
         
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Bag #', toString(tokenId), '", "description": "Loot is randomized adventurer gear generated and stored on chain. Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Loot in any way you want.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Lootbox #', toString(tokenId), '", "description": "Gear is randomized cypherpunk trappings generated and stored on chain. Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Gear in any way you want.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
@@ -1506,7 +1506,7 @@ contract Gear is ERC721Enumerable, ReentrancyGuard, Ownable {
         return string(buffer);
     }
     
-    constructor() ERC721("Loot", "LOOT") Ownable() {}
+    constructor() ERC721("Gear", "GEAR") Ownable() {}
 }
 
 /// [MIT License]
